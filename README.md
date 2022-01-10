0a9eb151007bf4f81ed43e4cd53dd1948c95f7b4
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-01-10 05:15:24 -0500

    added rotation and overwrote mul for vector

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tetris using Rust
 
-Current path: `https://youtu.be/74UYWFNfR64?t=3602`
+Current path: `https://youtu.be/74UYWFNfR64?t=5681`
--- src/engine/piece.rs
+++ src/engine/piece.rs
@@ -6,7 +6,14 @@ pub(super) struct Piece {
     pub rotation: Rotation,
 }
 
-impl Piece {}
+impl Piece {
+    const CELL_COUNT: usize = 4;
+
+    // todo is a divergent type, returns automatically any type
+    pub fn cells(&self) -> Option<impl Iterator<Item = Vector2<usize>>> {
+        self.kind.cells()
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Kind {
@@ -29,6 +36,21 @@ impl Kind {
         Self::S,
         Self::Z,
     ];
+
+    // adding static references for references, restricts references of values to the static
+    pub fn cells(&self) -> impl Iterator<Item = &'static Vector2<isize>> {
+        match self {
+            Kind::O => &[(0, 0), (0, 1), (1, 0), (1, 1)],
+            Kind::I => &[(-1, 0), (0, 0), (1, 0), (2, 0)],
+            Kind::T => &[(-1, 0), (0, 0), (1, 0), (0, 1)],
+            Kind::L => &[(-1, 0), (0, 0), (1, 0), (1, 1)],
+            Kind::J => &[(-1, 1), (-1, 0), (0, 0), (1, 0)],
+            Kind::S => &[(-1, 0), (0, 0), (0, 1), (1, 1)],
+            Kind::Z => &[(-1, 1), (0, 1), (0, 0), (1, 0)],
+        }
+        .iter()
+        .map(From::from)
+    }
 }
 
 pub enum Rotation {
@@ -36,4 +58,20 @@ pub enum Rotation {
     S,
     E,
     W,
+}
+
+impl<S> std::ops::Mul<Rotation> for Vector2<S>
+where
+    S: std::ops::Neg<Output = S>,
+{
+    type Output = Self;
+
+    fn mul(self, rotation: Rotation) -> Self::Output {
+        match rotation {
+            Rotation::N => self,
+            Rotation::S => Vector2::new(-self.x, -self.y),
+            Rotation::E => Vector2::new(self.y, -self.x),
+            Rotation::W => Vector2::new(-self.y, self.x),
+        }
+    }
 }
